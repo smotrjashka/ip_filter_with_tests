b49a00ca83e3755f2975646a31665d36c2642da0
nastiag <nastenhkag@gmail.com>
2021-11-28 18:41:07 +0200

    try test version

--- hello_test.cc
+++ hello_test.cc
@@ -1,9 +1,7 @@
 #include <gtest/gtest.h>
+#include "version.h"
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
-// Expect two strings not to be equal.
-EXPECT_STRNE("hello", "world");
-// Expect equality.
-EXPECT_EQ(7 * 6, 42);
+EXPECT_TRUE(version::getVersion()>=0);
 }
